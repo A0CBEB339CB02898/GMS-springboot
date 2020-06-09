38fff8dfd76a365ef4b7c38d30e2c01291c606db
OrionAAAA <38377286+Orionfirst@users.noreply.github.com>
2020-06-09 17:24:53 +0800

    tradingAPI

--- src/main/java/com/gms/controller/tradingAPI.java
+++ src/main/java/com/gms/controller/tradingAPI.java
@@ -5,6 +5,7 @@ import com.gms.mapper.tradingMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,14 @@ import java.util.List;
 public class tradingAPI {
     @Autowired
     tradingMapper tradingMapper;
-    List<trading> tradings;
+    private  List<trading> tradings;
 
     @GetMapping("/trading")
     public String trading(){
         String str=null;
+        tradings = tradingMapper.getAllTrading();
         str = tradings.get(1).getTradingID()+tradings.get(1).getCounterParty();
+        System.out.println(str);
         return str;
     }
 }
