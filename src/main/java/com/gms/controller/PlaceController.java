604c2c2e6ab0b08780c0707afd240284771ad3c5
PASSERFBER <2211319551@qq.com>
2020-06-10 20:42:09 +0800

    删除了两个mapper和两个controller

--- src/main/java/com/gms/controller/PlaceController.java
+++ src/main/java/com/gms/controller/PlaceController.java
@@ -1,5 +1,7 @@
 package com.gms.controller;
 
+import com.gms.entity.Appointment;
+import com.gms.entity.Charge;
 import com.gms.entity.Place;
 import com.gms.mapper.PlaceMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,20 +11,37 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * Created by PASSERFBER on 2020/6/10 17:46
+ * Created by PASSERFBER on 2020/6/10 18:08
  */
+
 @RestController
 public class PlaceController {
-  @Autowired
+    @Autowired
     PlaceMapper placeMapper;
-    private List<Place> placeList;
+     private List<Appointment> appointmentList;
+     private List<Place>  placeList;
+     private List<Charge> chargeList;
 
-    @GetMapping("/place")
+     @GetMapping("/place/appointment")
+    public String appointment(){
+         String str=null;
+         appointmentList = placeMapper.getAllAppointment();
+         str = appointmentList.get(1).getIdAppointment()+appointmentList.get(1).getIdPlace()+appointmentList.get(1).getStartAppointment()+appointmentList.get(1).getOverAppointment()+appointmentList.get(1).getUserId()+appointmentList.get(1).getCharacter()+appointmentList.get(1).getIdCharge();
+         return str;
+     }
+
+    @GetMapping("/place/charge")
+    public String Charge(){
+        String str=null;
+        chargeList=placeMapper.getAllCharge();
+        str = chargeList.get(1).getIdCharge()+chargeList.get(1).getIdPlace()+chargeList.get(1).getStartCharge()+chargeList.get(1).getOverCharge()+chargeList.get(1).getLight()+chargeList.get(1).getCost();
+        return str;
+    }
+    @GetMapping("/place/place")
     public String Place(){
         String string = null;
         placeList = placeMapper.getAllPlace();
         string = placeList.get(1).getIdPlace()+placeList.get(1).getPlaceName() + placeList.get(1).getLocation();
         return string;
     }
-
 }
--- src/main/java/com/gms/mapper/PlaceMapper.java
+++ src/main/java/com/gms/mapper/PlaceMapper.java
@@ -1,5 +1,7 @@
 package com.gms.mapper;
 
+import com.gms.entity.Appointment;
+import com.gms.entity.Charge;
 import com.gms.entity.Place;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
@@ -7,10 +9,17 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 /**
- * Created by PASSERFBER on 2020/6/10 17:38
+ * Created by PASSERFBER on 2020/6/10 18:06
  */
 @Repository
 public interface PlaceMapper {
+    @Select("select * from Appointment")
+    List<Appointment> getAllAppointment();
+
+    @Select("select * from Charge")
+    List<Charge> getAllCharge();
+
     @Select("select * from Place")
     List<Place> getAllPlace();
+
 }
