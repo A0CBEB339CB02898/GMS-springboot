a9c24572669e656e8d288d0f5b6b45bed8db64dc
YellowMaster106 <1060648472@qq.com>
2020-06-11 11:01:12 +0800

    Merge branch 'master' of C:\Users\Administrator\Desktop\代码缓存区\GMS-springboot with conflicts.

--- src/main/java/com/gms/mapper/EquipmentMapper.java
+++ src/main/java/com/gms/mapper/EquipmentMapper.java
@@ -1,7 +1,9 @@
 package com.gms.mapper;
 
 import com.gms.entity.Equipment;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,13 @@ import java.util.List;
 public interface EquipmentMapper {
     @Select("select * from Equipment")
     List<Equipment> getAllEquipment();
+
+    @Insert("insert into Equipment(equipmentId,equipmentName,equipmentCost)" +
+            "values(#{equipmentId},#{equipmentName},#{equipmentCost})")
+    public int insertEquipment(Equipment equipment);
+
+    @Update("update Equipment set equipmentStatus = #{equipmentStatus} " +
+            "where equipmentId = #{equipmentId}")
+    public int repairEquipment(Equipment equipment);
+
 }
