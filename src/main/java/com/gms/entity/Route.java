20cd2d5dff0acf6cfde9be3248489fa69cdd99bc
GHkmmm <hgmkk2299@icloud.com>
2020-06-15 18:12:46 +0800

    完成动态路由信息的返回

---
+++ src/main/java/com/gms/entity/Route.java
@@ -0,0 +1,49 @@
+package com.gms.entity;
+
+public class Route {
+    private String path;
+    private String component;
+    private String name;
+    private String icon;
+    private String redirect;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getRedirect() {
+        return redirect;
+    }
+
+    public void setRedirect(String redirect) {
+        this.redirect = redirect;
+    }
+}
