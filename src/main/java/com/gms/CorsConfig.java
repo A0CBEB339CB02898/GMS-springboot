ec97bc2a7d7ea8a975279ca9c337ad3c43832339
OrionAAAA <38377286+Orionfirst@users.noreply.github.com>
2020-06-22 13:39:18 +0800

    删除跨域配置

---
+++ src/main/java/com/gms/CorsConfig.java
@@ -0,0 +1,30 @@
+//package com.gms;
+//
+///**
+// * Created by Orion on 2020/6/21 21:06
+// */
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class CorsConfig {
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedOrigin("*"); // 1
+//        corsConfiguration.addAllowedHeader("*"); // 2
+//        corsConfiguration.addAllowedMethod("*"); // 3
+//        //最终的结果是可以 在跨域请求的时候获取同一个 session
+//        corsConfiguration.setAllowCredentials(true);
+//        return corsConfiguration;
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig()); // 4
+//        return new CorsFilter(source);
+//    }
+//}
