226eadb70452ab63080e83512e1245116205ec49
OrionAAAA <38377286+Orionfirst@users.noreply.github.com>
2021-01-18 17:13:05 +0800

    跨域配置

--- src/main/java/com/gms/GmsApplication.java
+++ src/main/java/com/gms/GmsApplication.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.gms.mapper")
-@RestController
 public class GmsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GmsApplication.class, args);
     }
